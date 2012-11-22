820b827df7c521c9848622cf679d01a1f9a1fb7c
Volker Grabsch <vog@notjusthosting.com>
2012-11-22 14:33:19 +0100

    Don't send unneeded arguments to XSLT processors

--- test/test.sh
+++ test/test.sh
@@ -2,16 +2,18 @@
 set -eu
 
 xslt_xsltproc () {
-    xsltproc --nonet -o $1 --stringparam action $3 --stringparam name "$4" $2
+    xsltproc --nonet -o $1 ${3:+'--stringparam'} ${3:+'action'} ${3:-} ${4:+'--stringparam'} ${4:+'name'} ${4:-} $2
 }
 xslt_xalan_cxx () {
-    xalan -validate -out $1 -in file://$(pwd)/$2 -param action "'$3'" -param name "'$4'"
+    xalan -validate -out $1 -in file://$(pwd)/$2 ${3:+'-param'} ${3:+'action'} ${3:+"'$3'"} ${4:+'-param'} ${4:+'name'} ${4:+"'$4'"}
 }
 xslt_saxon () {
-    java -classpath /usr/share/java/saxon.jar com.icl.saxon.StyleSheet -a -w2 -o $1 $2 action=$3 "name=${4:-''}"
+    java -classpath /usr/share/java/saxon.jar com.icl.saxon.StyleSheet \
+        -a -w2 -o $1 $2 ${3+"action=$3"} ${4:+"name=$4"}
 }
 xslt_xalan_j () {
-    java -classpath /usr/share/java/xalan2.jar org.apache.xalan.xslt.Process -SECURE -OUT $1 -IN $2 -PARAM action $3 -PARAM name "$4"
+    java -classpath /usr/share/java/xalan2.jar org.apache.xalan.xslt.Process \
+        -SECURE -OUT $1 -IN $2 ${3+'-PARAM'} ${3:+'action'} ${3:-} ${4:+'-PARAM'} ${4:+'name'} ${4:-}
 }
 
 trap 'rm -f tmp_output_5EZNkciv.sh' 0 INT QUIT
@@ -19,7 +21,7 @@ trap 'rm -f tmp_output_5EZNkciv.sh' 0 INT QUIT
 for xslt in xslt_xsltproc xslt_xalan_cxx xslt_saxon xslt_xalan_j; do
     echo "Testing with $xslt ..."
     for action in invalid http network update; do
-        $xslt tmp_output_5EZNkciv.sh ../config_sample.xml $action ''
+        $xslt tmp_output_5EZNkciv.sh ../config_sample.xml $action
         diff -u sample_$action.sh tmp_output_5EZNkciv.sh
     done
     for name in invalid private tcponly httponly complex; do
