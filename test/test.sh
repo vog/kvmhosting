863d633c2b27822c60fca0319d76c59797a05ec9
Volker Grabsch <vog@notjusthosting.com>
2012-11-22 11:43:51 +0100

    Sort XSLT processors by speed in test suite

--- test/test.sh
+++ test/test.sh
@@ -7,16 +7,16 @@ xslt_xsltproc () {
 xslt_xalan_cxx () {
     xalan -validate -xsl $1 -in $2 -param action "'$3'" -param name "'$4'"
 }
-xslt_xalan_j () {
-    java -classpath /usr/share/java/xalan2.jar org.apache.xalan.xslt.Process -SECURE -XSL $1 -IN $2 -PARAM action $3 -PARAM name "$4"
-}
 xslt_saxon () {
     java -classpath /usr/share/java/saxon.jar com.icl.saxon.StyleSheet -w2 $2 $1 "action=$3" "name=${4:-''}"
+}
+xslt_xalan_j () {
+    java -classpath /usr/share/java/xalan2.jar org.apache.xalan.xslt.Process -SECURE -XSL $1 -IN $2 -PARAM action $3 -PARAM name "$4"
 }
 
 trap 'rm -f tmp_output_5EZNkciv.sh' 0 INT QUIT
 
-for xslt in xslt_xsltproc xslt_xalan_cxx xslt_xalan_j xslt_saxon; do
+for xslt in xslt_xsltproc xslt_xalan_cxx xslt_saxon xslt_xalan_j; do
     echo "Testing with $xslt ..."
     for name in invalid private tcponly httponly complex; do
         $xslt ../kvmhosting.xsl ../config_sample.xml guest $name >tmp_output_5EZNkciv.sh
